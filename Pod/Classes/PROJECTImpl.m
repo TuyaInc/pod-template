30a0bbcc73e14d0a514cbee8986b9981086ae1d2
fangdong <fangdong@tuyasmart.com>
2018-09-13 20:58:29 +0800

    update demo

--- Pod/Classes/PROJECTImpl.m
+++ Pod/Classes/PROJECTImpl.m
@@ -6,9 +6,37 @@
 //  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import "HiSmartFMWKImpl.h"
+#import "TYModulPublic.h"
 
-#import "PROJECTImpl.h"
+@interface PROJECTImpl()<TYModuleProtocol>
+@end
 
 @implementation PROJECTImpl
+#pragma mark -
+#pragma mark - TYModuleTabProtocol
+//注册当前模块被作为TAB 存在着提供的输出能力
+//如果希望你的模块成为一个tab 请注册以下方法
 
+- (TYModuleTabMapping)registerModuleTab {
+    bool showBadge = false;  //暂时可以忽略 直接false即可
+    bool selected = false;   //暂时可以忽略 直接false即可
+
+    NSString *viewController = @"you view controller";
+    NSString *pageTitle = @"title";
+    NSString *normalImage = @"img_tab_normal";
+    NSString *selectedImage = @"img_tab_selected";
+
+    TYModuleTabMapping mapping = {
+        showBadge,
+        selected,
+        viewController,
+        pageTitle,
+        normalImage,
+        selectedImage
+    };
+
+    return mapping;
+}
 @end
